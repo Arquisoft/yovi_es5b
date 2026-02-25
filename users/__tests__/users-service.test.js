04d956404d9540850968389bad03fd98b42ef79c
NachoTS <UO245469@uniovi.es>
2026-02-25 20:32:29 +0100

    users: añadir prueba unitaria /getusers.
    Añadir prueba unitaria para la ruta de /getusers.
    
    Ahora mismo es muy básica; se debe implementar correctamente.

--- users/__tests__/users-service.test.js
+++ users/__tests__/users-service.test.js
@@ -2,6 +2,7 @@ import { describe, it, expect, afterEach, vi } from 'vitest'
 import request from 'supertest'
 import app from '../users-service.js'
 
+// TODO: createuser: implementar correctamente
 describe('POST /createuser', () => {
     afterEach(() => {
         vi.restoreAllMocks()
@@ -16,5 +17,21 @@ describe('POST /createuser', () => {
         expect(res.status).toBe(400)
         //expect(res.body).toHaveProperty('message')
         //expect(res.body.message).toMatch(/Hello Pablo! Welcome to the course!/i)
+    })
+})
+
+// TODO: getusers: implementar correctamente
+describe('GET /getusers', () => {
+    afterEach(() => {
+        vi.restoreAllMocks()
+    })
+
+    it('gets all users of the application', async () => {
+        const res = await request(app)
+            .get('/getusers')
+            .send()
+            .set('Accept', 'application/json')
+
+        expect(res.status).toBe(500)
     })
 })
