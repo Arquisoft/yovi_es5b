ee8f0ecb76ea550efbf10da979f7ee41790ee792
NachoTS <UO245469@uniovi.es>
2026-03-11 22:15:06 +0100

    Revert "Fix conexión con base de datos 2 #80"
    This reverts commit ebbb7599e2186da5d2e93b08ec20d6bbe4503b11.

--- database/init.sql
+++ database/init.sql
@@ -1,10 +1,7 @@
 -- Resetear permisos de root para todas las direcciones
-USE mysql;
-DELETE FROM user WHERE User='root' AND Host NOT IN ('localhost', '%');
-update user set host='%' where user='root';
-ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'ADMSIS123$';
-CREATE USER IF NOT EXISTS 'root'@'%' IDENTIFIED WITH mysql_native_password BY 'ADMSIS123$';
+DELETE FROM mysql.user WHERE User='root' AND Host != 'localhost';
+ALTER USER 'root'@'localhost' IDENTIFIED BY 'ADMSIS123$';
+CREATE USER IF NOT EXISTS 'root'@'%' IDENTIFIED BY 'ADMSIS123$';
 GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
 GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' WITH GRANT OPTION;
-
 FLUSH PRIVILEGES;
