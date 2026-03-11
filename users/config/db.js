543706d39f7dc1c2930581623f3318211a08ba35
NachoTS <UO245469@uniovi.es>
2026-03-11 23:53:15 +0100

    Revert "user: prueba de credenciales para despliegue"
    This reverts commit 9b80ba7d3a1f4cbda2e7c3397da56227ba739163.

--- users/config/db.js
+++ users/config/db.js
@@ -1,9 +1,9 @@
 import { Sequelize } from 'sequelize';
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME ?? "yovi-es5b-prod",
+  process.env.DB_NAME ?? "yovi-es5b-dev",
   process.env.DB_USER ?? "root",
-  process.env.DB_PASSWORD ?? "FTifXyc@#nZ5K9DPJ@tsgTTLaHoZtj",
+  process.env.DB_PASSWORD ?? "ADMSIS123$",
   {
     host: "172.17.0.1", // dirección interna de host de docker
     dialect: 'mysql',
