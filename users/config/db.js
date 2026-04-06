698ceac9b4a00dd3ba121be53284b3c9ac0fef78
NachoTS <UO245469@uniovi.es>
2026-04-06 17:16:38 +0200

    docker-compose: definir variables de users correctamente.

--- users/config/db.js
+++ users/config/db.js
@@ -1,9 +1,9 @@
 import { Sequelize } from 'sequelize';
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME ?? "yovi-es5b-dev",
-  process.env.DB_USER ?? "root",
-  process.env.DB_PASSWORD ?? "ADMSIS123$",
+  process.env.DB_NAME ? process.env.DB_NAME : "yovi-es5b-dev",
+  process.env.DB_USER ? process.env.DB_USER : "root",
+  process.env.DB_PASSWORD ? process.env.DB_PASSWORD : "ADMSIS123$",
   {
     host: process.env.DB_HOST ?? "127.0.0.1", // dirección interna de host de docker
     dialect: 'mysql',
